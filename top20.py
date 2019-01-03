69cf1eb2b0e8db3dae476ca8d0e9a242f6511d21
justong86 <44458988+justong86@users.noreply.github.com>
2019-01-03 17:10:08 +0800

    Update top20.py

--- top20.py
+++ top20.py
@@ -24,7 +24,7 @@ def get_top():
         tops = re.findall(data_compile, str(data))
         top1_name =tops[0][0]
         top1_num = tops[0][2]
-        if top1_name == "曲鹏舒":
+        if top1_name == "张三":
             top1_name = tops[1][0]
             top1_num = tops[1][2]
         print("第一竞争者：%s,票数:%s" % (top1_name ,top1_num))
@@ -32,7 +32,7 @@ def get_top():
         return top1_num
     time.sleep(5)
     get_top()
-
+# 改进了上面的方法，可以指定监控的名次，默认第一名
 def get_top2(i = 1):
     url='http://t.dramastar.org/zlist.php'
     response_url = requests.get(url)
@@ -43,7 +43,8 @@ def get_top2(i = 1):
         tops = re.findall(data_compile, str(data))
         top2_name =tops[i-1][0]
         top2_num = tops[i-1][2]
-        if top2_name == "曲鹏舒":
+        # 这里要排除自己的名字，要不然自己跟自己挣就没意思了        
+        if top2_name == "张三":
             top2_name = tops[i][0]
             top2_num = tops[i][2]
         print("第%s竞争者：%s,票数:%s" % (i,top2_name ,top2_num))
@@ -52,8 +53,9 @@ def get_top2(i = 1):
     time.sleep(5)
     get_top()
 
+# 获取指定ID的得票情况，其实是从SpiderDSS.py里拿出来的方法
 def spider_one(id):
-    url_web="http://t.dramastar.org/{}.html".format(id)
+    url_web="http://????????????.org/{}.html".format(id)
     response_web=requests.get(url=url_web,headers=header)
     soup = bs4.BeautifulSoup(response_web.text,'html.parser')
     sp_div = soup.find('div',class_ = 'teacher_info_yc')
